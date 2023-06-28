ab4e09098098179da50cd778397bcbd01d8c5c21
zmit09 <133233834+zmit09@users.noreply.github.com>
2023-06-28 10:45:37 -0230

    completed qap1

---
+++ http.js
@@ -0,0 +1,9 @@
+// The HTTP module can create an HTTP server that listens to server ports and gives a response back to the client //
+
+var http = require('http');
+
+//create a server object:
+http.createServer(function (req, res) {
+  res.write('Hello World!'); //write a response to the client
+  res.end(); //end the response
+}).listen(8080); //the server object listens on port 8080
---
+++ os.js
@@ -0,0 +1,5 @@
+// Get information about the computer's operating system //
+
+var os = require('os');
+console.log("Platform: " + os.platform()); // printing the platform information to the console //
+console.log("Architecture: " + os.arch()); // printing the architecture information to the console //
